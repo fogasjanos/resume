c0c016662d6fd9007e7e58cb7e16f02853309a07
fogasjanos <fogasjanos@gmail.com>
2025-02-26 23:20:55 +0100

    Finetune printing style, hobbies and languages should shown up in one line to save print space

--- src/app/interfaces/label-values.interface.ts
+++ src/app/interfaces/label-values.interface.ts
@@ -3,5 +3,6 @@ import { Styleable } from './styleable.interface';
 export interface LabelValues extends Styleable {
   readonly label: string;
   readonly values: string[];
-  readonly disabled?: boolean; 
+  readonly disabled?: boolean;
+  readonly className?: string;
 }
